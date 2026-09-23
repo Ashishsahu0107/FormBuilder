0b406c7ee4e7bd31ee553ec1791af6929e22d147
Ashishsahu0107 <ashishsahu01072005@gmail.com>
2026-09-23 23:38:45 +0530

    refactor: remove unused Prisma client scripts and related functionality

--- server/test_zod.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { z } = require('zod')
-
-const updateVersionSchema = z.object({
-  schema: z.any(),
-})
-
-const result = updateVersionSchema.safeParse({ schema: { elements: [] } })
-console.log('Result:', result)
